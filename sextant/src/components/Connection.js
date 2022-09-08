617afa2475d4e546ce52be5c5af1486453906182
ALMA_DEV914 <aungonalna58@gamil.com>
2022-09-07 18:11:22 -0700

    connect websocket

---
+++ sextant/src/components/Connection.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Connection extends Component {
+  // instance of websocket connection as a class property
+  ws = new WebSocket("ws://localhost:55455/ws");
+
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log("connected");
+    };
+
+    this.ws.onmessage = (evt) => {
+      // listen to data sent from the websocket server
+      const message = JSON.parse(evt.data);
+      this.setState({ dataFromServer: message });
+      console.log(message);
+      
+    };
+
+    this.ws.onclose = () => {
+      console.log("disconnected");
+      // automatically try to reconnect on connection loss
+    };
+  }
+
+  
+  render() {
+     return(
+        <div>{this.message}</div>
+     )
+    
+  }
+}
+
+export default Connection;
--- sextant/src/components/Exhibit.js
+++ sextant/src/components/Exhibit.js
@@ -4,6 +4,7 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Banner from "./Banner";
 import GetUserv4Info from "./v4ipAddress";
 import GetUserv6Info from "./v6IpAddress";
+import Connection from "./Connection";
 
 class Exhibit extends Component {
   render() {
@@ -14,7 +15,7 @@ class Exhibit extends Component {
         </div>
         <Container className="mt-4">
           <Row>
-            <Col className="col-6">
+            <Col className="col-4">
               <Card>
                 <Card.Header>User v4 Ip address</Card.Header>
                 <Card.Body>
@@ -26,13 +27,25 @@ class Exhibit extends Component {
                 </Card.Body>
               </Card>
             </Col>
-            <Col className="col-6">
+            <Col className="col-4">
               <Card>
                 <Card.Header>User v6 Ip Address</Card.Header>
                 <Card.Body>
                   <Card.Title>Special title treatment</Card.Title>
                   <Card.Text>
                     <GetUserv6Info />
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="col-4">
+              <Card>
+                <Card.Header>Stream Data from a Networked Service</Card.Header>
+                <Card.Body>
+                  <Card.Title>All Events</Card.Title>
+                  <Card.Text>
+                    <Connection/>
                   </Card.Text>
                   <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
